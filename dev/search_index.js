8179bef4d6b6b3574fcecf9f99d04d323a99561b
Documenter.jl <documenter@juliadocs.github.io>
2026-02-27 07:45:22 +0000

    build based on 952491b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"api/#API-Reference","page":"API Reference","title":"API Reference","text":"","category":"section"},{"location":"#GraphNetSim","page":"Home","title":"GraphNetSim","text":"(Image: Docs) (Image: Build Status) (Image: ColPrac: Contributor's Guide on Collaborative Practices for Community Packages) (Image: Code Style: Blue)","category":"section"}]
+}
